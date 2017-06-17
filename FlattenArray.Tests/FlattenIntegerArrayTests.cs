147be07a4666bcac09854cde3e7d37b2144cbf33
Shameem <shameema@wakanow.com>
2017-06-17 12:19:09 +0400

    Add project files.

---
+++ FlattenArray.Tests/FlattenIntegerArrayTests.cs
@@ -0,0 +1,63 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FlattenIntegerArrayTests.cs" author="Shameem Ahamed">
+//   Copyright (c) All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.IO;
+using NUnit.Framework;
+using System.Linq;
+
+namespace FlattenArray.Tests
+{
+    [TestFixture]
+    public class FlattenIntegerArrayTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            flattenIntegerArrays = new FlattenIntegerArrays();
+        }
+
+        private FlattenIntegerArrays flattenIntegerArrays;
+
+        [TestCase(1)]
+        [TestCase(1, 2)]
+        [TestCase(1, 2, new object[] { 3 })]
+        [TestCase(1, 2, new object[] { 3, new object[] { 6, 7 } })]
+        [TestCase(1, 2, new object[] { 3 }, 4, new object[] { 5, new object[] { 6, 7 } }, 8)]
+        [TestCase(1, 2, new object[] { 3 }, 4, new object[] { 5, new object[] { 6, 7 } }, 8)]
+        public void GetFlattenedArray_ValidArrays_ReturnsFlattenedArray(object[] inputArrays)
+        {
+            var result = flattenIntegerArrays.GetFlattenedArray(inputArrays);
+
+            Assert.IsTrue(result.Count() > 0);
+        }
+
+        [TestCase(1, 2, new object[] { "string" }, 4, new object[] { 5, new object[] { 6, 7 } }, 8)]
+        [TestCase(1.001, 2, new object[] { "3" }, 4, new object[] { 5, new object[] { 6, 7 } }, 8)]
+        [TestCase(1234567891234, 2, new object[] { 3 }, 4)]
+        [TestCase(1234567891234, 2, new[] { true }, 4)]
+        public void GetFlattenedArray_InvalidDataTypeInArray_ThrowsException(object[] inputArrays)
+        {
+            Assert.Catch<InvalidDataException>(() => flattenIntegerArrays.GetFlattenedArray(inputArrays));
+        }
+
+        [TestCase(1, 2, new object[] { }, 4, new object[] { 5, new object[] { 6, 7 } }, 8)]
+        public void GetFlattenedArray_BlankInArray_ReturnsResult(object[] inputArrays)
+        {
+            var result = flattenIntegerArrays.GetFlattenedArray(inputArrays);
+
+            Assert.IsTrue(result.Count() > 0);
+        }
+
+        [TestCase(1, new[] { 2, 3 }, 4)]
+        [TestCase(1, 2, new int[] { }, 4, new object[] { 5, new[] { 6, 7 } }, 8)]
+        public void GetFlattenedArray_IntegerArrays_ReturnsResult(object[] inputArrays)
+        {
+            var result = flattenIntegerArrays.GetFlattenedArray(inputArrays);
+
+            Assert.IsTrue(result.Count() > 0);
+        }
+    }
+}
---
+++ FlattenArray/FlattenIntegerArrays.cs
@@ -0,0 +1,63 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FlattenIntegerArrays.cs" author="Shameem Ahamed">
+//   Copyright (c) All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.IO;
+
+namespace FlattenArray
+{
+    /// <summary>
+    /// Flatten Integer Arrays
+    /// </summary>
+    public class FlattenIntegerArrays
+    {
+        private const string InvalidDataExceptionMessage = "Item is not a valid Integer";
+
+        /// <summary>
+        /// Gets the flattened array.
+        /// </summary>
+        /// <param name="inputArray">The input array.</param>
+        /// <returns>Array of Integers</returns>
+        public int[] GetFlattenedArray(object[] inputArray)
+        {
+            var result = new List<int>();
+
+            Flatten(inputArray, result);
+
+            return result.ToArray();
+        }
+
+        private static void Flatten(object[] inputArray, ICollection<int> result)
+        {
+            foreach (var arrayItem in inputArray)
+            {
+                if (arrayItem is int)
+                {
+                    result.Add((int)arrayItem);
+                }
+                else
+                {
+                    var array = arrayItem as object[];
+                    if (array != null)
+                    {
+                        Flatten(array, result);
+                    }
+                    else if (arrayItem is int[])
+                    {
+                        foreach (var item in (int[])arrayItem)
+                        {
+                            result.Add(item);
+                        }
+                    }
+                    else
+                    {
+                        throw new InvalidDataException(InvalidDataExceptionMessage);
+                    }
+                }
+            }
+        }
+    }
+}
